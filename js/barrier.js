14b6cb0c03dd4980b706f483cbfd9511f91f8980
Augusto Calaca <augusto@Calaca.Augusto>
2019-04-21 01:04:12 -0300

    add barrier

---
+++ js/barrier.js
@@ -0,0 +1,12 @@
+import { newElement } from "./newElement.js";
+
+export const Barrier = function(reverse = false) {
+  console.log('callll');
+  this.elem = newElement('div', 'barrier');
+  const barrierBorder = newElement('div', 'barrier-border');
+  const barrierBody = newElement('div', 'barrier-body');
+
+  this.elem.appendChild(reverse ? barrierBody : barrierBorder);
+  this.elem.appendChild(reverse ? barrierBorder : barrierBody);
+  this.setHeight = height => barrierBody.style.height = `${height}px`;
+}
