8a04551260d4626c08e92e9921efb505f5ad076c
Augusto Calaca <augusto@Calaca.Augusto>
2019-05-11 01:24:52 -0300

    add call for overlap, colision and progress

--- js/main.js
+++ js/main.js
@@ -3,17 +3,32 @@ import { Barrier } from './barrier.js';
 import { TopDownBarriers } from './topDownBarriers.js';
 import { AllBarriers } from './allBarriers.js';
 import { Bird } from './bird.js';
+import { overlap } from './overlap.js';
+import { collision } from './collision.js';
+import { Progress } from './progress.js';
 
 const fp = document.querySelector('[flappy]');
 const bird = new Bird(fp.clientHeight);
-fp.appendChild(bird.elem);
 
-const barriers = new AllBarriers(fp.clientHeight, 200, fp.clientWidth, 400);
+const progress = new Progress();
+
+const barriers = new AllBarriers(fp.clientHeight, 200, fp.clientWidth, 400, () => {
+  const progressValue = parseInt(document.querySelector('.progress').innerHTML);
+  progress.pointUpdate(progressValue + 1);
+});
+
+fp.appendChild(progress.elem);  
+fp.appendChild(bird.elem);
 barriers.all.forEach(topDown => {
   fp.appendChild(topDown.elem);
-})
+});
 
-setInterval(() => {
+
+const temp = setInterval(() => {
   barriers.animation();
   bird.animation();
-}, 20)
+
+  if(collision(bird, barriers)) {
+    clearInterval(temp);
+  }
+}, 20);
